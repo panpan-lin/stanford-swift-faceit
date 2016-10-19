06df6719cb0938cfcd524632a6b1f683cce5d996
Panpan Lin <panpanli@ie.ibm.com>
2016-10-19 22:36:24 +0100

    check in teacher demo code from video 4

---
+++ Facelet/FaceViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Facelet
+//
+//  Created by Panpan Lin on 19/10/2016.
+//  Copyright © 2016 IBM. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+}
+
